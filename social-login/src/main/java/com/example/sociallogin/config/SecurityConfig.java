1626f23bdee85e57db320904481b74614c0b549f
DESKTOP-A9ID9FD\KimHyeon <k97h07o11@gmail.com>
2023-07-31 00:53:06 +0900

    소셜 로그인 구현

--- social-login/src/main/java/com/example/sociallogin/config/SecurityConfig.java
+++ social-login/src/main/java/com/example/sociallogin/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.sociallogin.config;
 
+import com.example.sociallogin.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,7 +9,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,6 +24,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling(e -> e
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 )
-                .oauth2Login();
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService)
+                        )
+                );
     }
 }
